c8144247c0422d2481768fde244e6f9d2e254aef
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-09-12 10:58:41 -0500

    completed for loop odd/even exercise in for_loops.js

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,9 +1,20 @@
 "use strict"
 
-function showMultiplicationTable(number){
-	for(let i = 1; i <= 10; i++){
-		console.log(`${number} x ${i} = ${number*i}`)
-		// return number * i;
+// function showMultiplicationTable(number){
+// 	for(let i = 1; i <= 10; i++){
+// 		console.log(`${number} x ${i} = ${number*i}`)
+// 		// return number * i;
+// 	}
+// }
+// showMultiplicationTable((7))
+
+
+for(let i = 1; i <= 10; i++){
+	let random = Math.floor(Math.random() * 180) + 20;
+	// console.log(random)
+	if(random % 2 === 0){
+		console.log(`${random} is even`)
+	} else {
+		console.log(`${random} is odd`)
 	}
 }
-showMultiplicationTable((7))
