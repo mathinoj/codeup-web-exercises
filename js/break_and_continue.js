dba517d1ae48e586ff5be1e95aa1839c500d0709
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-09-15 09:51:19 -0500

    refactored break/continue exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,25 +1,36 @@
 "use strict"
 
-let chooseOdd = Number(prompt("Choose an odd number between 1 and 50!"))
-// console.log('choose: ' +chooseOdd)
-// let parseEntry = parseInt(chooseOdd)
-console.log(`Numb to skip ${chooseOdd}`)
+// let chooseOdd = Number(prompt("Choose an odd number between 1 and 50!"))
+// console.log(`Numb to skip ${chooseOdd}`)
+//
+// for(let i=1; i <= 50; i++){
+// 	if(i === chooseOdd){
+// 		console.log(`Skipping number: ${chooseOdd}`)
+// 	} else if(i % 2 !== 0 ){
+// 		console.log("Here is odd number: " +i)
+// 	}
+// }
 
-for(let i=1; i <= 50; i++){
-	// console.log('choose: ' +chooseOdd)
-	// console.log(i)
-	// console.log(parseEntry)
-	// console.log(`Numb to skip ${chooseOdd}`)
-	if(i === chooseOdd){
-		console.log(`Skipping number: ${chooseOdd}`)
-		// break;
-	} else if(i % 2 !== 0 ){
-		console.log("Here is odd number: " +i)
-		// continue;
-		// console.log(`Numb to skip ${chooseOdd}`)
+function numSkip() {
+	let userInput;
+
+	while (true) {
+		userInput = prompt("Give me an odd number between 1 & 50");
+
+		if (userInput % 2 !== 0 && userInput > 0 && userInput < 51) {
+			break;
+		}
 	}
-	// console.log(i)
-	// console.log("Here is odd number: " +i)
 
+	console.log(`Number to skip is ${userInput}`);
+
+	for (let i = 1; i < 50; i += 2) {
+		if (i == userInput) {
+			console.log(`Yikes! skipping number: ${i}`);
+			continue;
+		}
+		console.log(`Here is an odd number: ${i}`);
+	}
 }
+numSkip()
 
---
+++ js/iterating.js
@@ -0,0 +1,78 @@
+(function(){
+	"use strict";
+
+	/**
+	 * TODO:
+	 * Create an array of 4 people's names and store it in a variable called
+	 * 'names'.
+	 */
+let names = ['Jo', 'Moe', 'Beau', 'Eddie'];
+	/**
+	 * TODO:
+	 * Create a log statement that will log the number of elements in the names
+	 * array.
+	 */
+console.log(names.length)
+	/**
+	 * TODO:
+	 * Create log statements that will print each of the names individually by
+	 * accessing each element's index.
+	 */
+	console.log(`This is: ${names[0]}`)
+	console.log(`This is: ${names[1]}`)
+	console.log(`This is: ${names[2]}`)
+	console.log(`This is: ${names[3]}`)
+
+
+	/**
+	 * TODO:
+	 * Write some code that uses a for loop to log every item in the names
+	 * array.
+	 */
+	let x = names.length
+	for(let i = 0; i < x; i++) {
+		console.log(names[i])
+	}
+	//
+	// let y = names.length
+	// for(let i = 0; i < y; i++) {
+	// 	console.log(names[i].length)
+	// }
+
+	/**
+	 * TODO:
+	 * Refactor your above code to use a `forEach` loop
+	 */
+	names.forEach((name => console.log(`Here is a name: ${name}`)))
+
+	/**
+	 * TODO:
+	 * Create the following three functions, each will accept an array and
+	 * return an an element from it
+	 * - first: returns the first item in the array
+	 * - second: returns the second item in the array
+	 * - last: returns the last item in the array
+	 *
+	 * Example:
+	 *  > first([1, 2, 3, 4, 5]) // returns 1
+	 *  > second([1, 2, 3, 4, 5]) // returns 2
+	 *  > last([1, 2, 3, 4, 5]) // return 5
+	 */
+	let first = [1, 2, 3, 4, 5]
+	let second = [1, 2, 3, 4, 5]
+	let last = [1, 2, 3, 4, 5]
+	function one(){
+		console.log(first[0])
+	}
+	one()
+
+	function two(){
+		console.log(second[1])
+	}
+	two()
+
+	function third(){
+		console.log(last[4])
+	}
+	third()
+})();
