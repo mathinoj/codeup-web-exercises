1d9277fda0b92ff64f50fa6e41f4308a2dce12d3
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-10-23 11:28:21 -0500

    Created weathermap css and linked it to html

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,6 @@
 'use strict'
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?` +
+	`lat=29.426825118534886&lon=-98.48948239256946` +
+	`&appid=${WEATHERMAP_API_KEY}`)
+	.then( data => console.log(data));
