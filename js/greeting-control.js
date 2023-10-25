ee1c6289491c4b903839beeabc374dd5e4052864
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-10-25 15:57:07 -0500

    Completed greeting exercise

---
+++ js/greeting-control.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import {randGreet} from './greeting-logic.js'
+
+function main() {
+	document.querySelector('#greetButt').addEventListener('click', greetUser)
+}
+
+	function greetUser(){
+		let firstName = document.querySelector('#fName').value
+		console.log(firstName)
+
+		let greetThem = randGreet() + " " + firstName;
+
+		document.querySelector('.greeting').innerText =
+			greetThem;
+}
+main();
--- js/greeting-logic.js
+++ js/greeting-logic.js
@@ -1,3 +1,7 @@
+'use strict'
+
+import randomNumber from "./number-utils.js";
+
 const greetings =
 	[
 		"Hello",
@@ -21,3 +25,14 @@ const greetings =
 		"How are you",
 		"Pleased to meet you"
 	];
+// In greeting-logic.js, put the following array of greetings and create a function that returns a random greeting. Make sure to export what you need from this file and import the random number function from numberUtils.
+
+export function randGreet(){
+	let indexNumb = randomNumber(0, (greetings.length - 1))
+	// for(let i=0; i < greetings.length; i++){
+	// console.log(greetings[indexNumb])
+	return greetings[indexNumb]
+	// }
+}
+// randGreet()
+
