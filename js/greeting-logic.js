6464183881df4306cd2669e36e6b34fd2774d3b8
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-10-25 14:47:44 -0500

    Created greeting js files and html, and added array to greeting-logic

---
+++ js/greeting-logic.js
@@ -0,0 +1,23 @@
+const greetings =
+	[
+		"Hello",
+		"Hi",
+		"Hey there",
+		"Greetings",
+		"Salutations",
+		"Howdy",
+		"Yo",
+		"Bonjour",
+		"Hola",
+		"Ciao",
+		"Namaste",
+		"G'day",
+		"What's up",
+		"Sup",
+		"How's it going",
+		"Nice to meet you",
+		"Welcome",
+		"Hey",
+		"How are you",
+		"Pleased to meet you"
+	];
